08d52c16abe7e1ecb80b4373f4901ccadfbae246
Kaif Shaikh <kaifshaikh999@gmail.com>
2025-11-25 10:00:21 +0530

    feat: added createProblem controller with Judge0 validation logic
    - added createProblem controller for creating new coding problems
    - integrated Judge0 batch submission to verify reference solution against testcases
    - added admin role check inside the controller
    - added database save for validated problem data

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-
+import problemsRoutes from './routes/problem.routes.js';
 import authRoutes from './routes/auth.routes.js';
+
 
 
 
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/problems", problemsRoutes);
 
 
 app.listen(PORT, () => {
